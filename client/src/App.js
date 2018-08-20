c0967858b6ee30171d32beaf3916535ea77c7763
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-08-20 15:30:22 -0500

    Add mini drawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import MiniDrawer from './components/MiniDrawer';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({users}, ()=> console.log('Users fetched...', users)));
-  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        <ul>
-          {this.state.users.map(user => 
-            <li key={user.id}>{user._id}<Button variant="contained" color="primary">Delete</Button></li>
-          )}
-        </ul>
-        </p>       
+        <MiniDrawer />       
       </div>
     );
   }
