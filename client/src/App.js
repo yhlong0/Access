19a021c176e713d17133e969f591ba2a345b0348
yhlong0 <yhlong2@gmail.com>
2018-08-19 22:19:46 -0500

    test api

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  ComponentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({users}, ()=> console.log('Users fetched...', users)));
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +26,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {this.state.users.map(user => 
+            <li key={user._id}>{user.firstname}</li>
+          )}
+        </ul>
       </div>
     );
   }
