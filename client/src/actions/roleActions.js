54b61b099d53df91b609f23e10f39dee80353f68
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-08-30 16:59:37 -0500

    create fetch role action, add react-redux.

--- client/src/actions/roleActions.js
+++ client/src/actions/roleActions.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function fetchRoles() {
     return {
         type: "FETCH_ROLES_FULFILLED",
@@ -5,5 +7,17 @@ export function fetchRoles() {
             name: 'Customer Support',
             description: 'Customer support level 1',
         }
+    };
+};
+
+export function fetchRole() {
+    return function(dispatch) {
+        axios.get('/roles')
+            .then((res) => {
+                dispatch({type: 'FETCH_ROLE_FULFILLED', payload: res.data})
+            })
+            .catch((err) => {
+                dispatch({type: 'FETCH_ROLE_REJECTED', payload: err})
+            });
     };
 };
