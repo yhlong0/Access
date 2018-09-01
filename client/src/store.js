68b3058e79b68acc1ac506534b18415135a757fc
yhlong0 <yhlong2@gmail.com>
2018-08-31 22:31:01 -0500

    using redux promise middleware for promise payload

--- client/src/store.js
+++ client/src/store.js
@@ -6,6 +6,6 @@ import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk, logger, promise());
 
 export default createStore(reducer, middleware);
