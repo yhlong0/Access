158392f1abb3d57323d7aa79c62b7d22272eaa0a
hailong <yhlong2@gmail.com>
2018-09-07 12:46:59 -0500

    add date for system access

--- client/src/components/Molecules/SystemColumn.js
+++ client/src/components/Molecules/SystemColumn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
+import * as moment from 'moment';
 
 
 const styles = theme => ({
@@ -17,9 +18,10 @@ let SystemColumn = props => {
     const { classes, sysAccess, removeAccess, userId } = props;
   
     const sysAccessList = sysAccess.map((system) => {
+        const time = moment(system.accessDate).format('MM/DD/YYYY');
         return (
             <Chip
-                label={system.name}
+                label={`${system.name} ${time}`}
                 className={classes.chip}
                 onDelete={() => removeAccess(system._id, userId)}
                 key={system._id}
