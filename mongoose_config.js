bcc7139ecdccab00975162e74d4b003e8ae63492
yhlong0 <yhlong2@gmail.com>
2018-09-09 00:13:13 -0500

    Create test db and switch db based on node env

--- mongoose_config.js
+++ mongoose_config.js
@@ -1,7 +1,15 @@
-//let config = require('./config/test');
-//const mongoose = require('mongoose');
+const config = require('./config/test');
 const mongoose = require('mongoose').set('debug', true);
-
-mongoose.connect('mongodb://localhost:27017/access', {useNewUrlParser: true});
+console.log(config.mongoURI[process.env.NODE_ENV]);
+mongoose.connect(
+    config.mongoURI[process.env.NODE_ENV], 
+    {useNewUrlParser: true}, 
+    function(err, res) {
+        if(err) {
+            console.log('Error connecting to db. ' + err);
+        } else {
+            console.log('Connected to DB: ' + config.mongoURI[process.env.NODE_ENV]);
+        }
+});
 
 exports.mongoose = mongoose;
