697ee670d093448e44705be8532b22c62878cb98
hailong <yhlong2@gmail.com>
2018-09-14 14:51:08 -0500

    call next function

--- client/src/redux/middleware/app/systems.js
+++ client/src/redux/middleware/app/systems.js
@@ -3,17 +3,17 @@ import { API_SUCCESS, apiRequest } from "../../actions/api.actions";
 import { setLoader } from '../../actions/ui.actions';
 import { API } from '../../constants/constants';
 
-export const systemsMiddleware = ({dispatch}) => next => action => {
+export const systemsMiddleware = () => next => action => {
     next(action);
 
     switch (action.type) {
         case FETCH_SYSTEMS:
-            dispatch(apiRequest(null, 'GET', API.SYSTEMS, SYSTEMS));
-            dispatch(setLoader(true));
+            next(apiRequest(null, 'GET', API.SYSTEMS, SYSTEMS));
+            next(setLoader(true));
             break;
         case `${SYSTEMS} ${API_SUCCESS}`:
-            dispatch(setSystems(action.payload));
-            dispatch(setLoader(false));
+            next(setSystems(action.payload));
+            next(setLoader(false));
             break;
     }
 };
