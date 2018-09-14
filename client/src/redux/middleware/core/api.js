0864ec970fcf04570dbc02049ff04755cc98df17
yhlong0 <yhlong2@gmail.com>
2018-09-13 21:46:52 -0500

    add api middleware

---
+++ client/src/redux/middleware/core/api.js
@@ -0,0 +1,14 @@
+import { API_REQUEST, apiError, apiSucess } from '../../actions/api.actions';
+
+export const apiMiddleware = ({dispatch}) => next => action => {
+    next(action);
+
+    if(action.type === API_REQUEST) {
+        const { entity, method, url } = action.payload.meta;
+
+        fetch(url, {method})
+            .then( res => res.json())
+            .then( data => apiSucess(data, entity))
+            .catch( error => apiError(error, entity))
+    }
+}
