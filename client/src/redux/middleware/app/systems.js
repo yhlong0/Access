79a8e118ba251852e6b48b1aa43d0c47451bd0a5
hailong <yhlong2@gmail.com>
2018-09-14 14:58:32 -0500

    finish system middleware

--- client/src/redux/middleware/app/systems.js
+++ client/src/redux/middleware/app/systems.js
@@ -1,7 +1,8 @@
 import { SYSTEMS, FETCH_SYSTEMS, setSystems } from '../../actions/systems.actions';
-import { API_SUCCESS, apiRequest } from "../../actions/api.actions";
+import { API_SUCCESS, API_ERROR, apiRequest } from "../../actions/api.actions";
 import { setLoader } from '../../actions/ui.actions';
 import { API } from '../../constants/constants';
+import { setNotification } from '../../actions/notification.actions';
 
 export const systemsMiddleware = () => next => action => {
     next(action);
@@ -13,7 +14,14 @@ export const systemsMiddleware = () => next => action => {
             break;
         case `${SYSTEMS} ${API_SUCCESS}`:
             next(setSystems(action.payload));
-            next(setLoader(false));
+            next(setLoader({ state: false, entity: SYSTEMS }));
+            break;
+        case `${SYSTEMS} ${API_ERROR}`: 
+            next(setNotification({
+                message: action.payload.message, 
+                entity: SYSTEMS
+            }));
+            next(setLoader({ state: false, entity: SYSTEMS}));
             break;
     }
 };
