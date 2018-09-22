fe4390006ca8f61851707a7fcb58228411a84954
yhlong0 <yhlong2@gmail.com>
2018-09-21 22:41:22 -0500

    user middleware

--- client/src/redux/middleware/app/users.js
+++ client/src/redux/middleware/app/users.js
@@ -1,19 +1,22 @@
 import { USERS, FETCH_USERS, setUsers } from '../../actions/users.actions';
-import { API_SUCCESS, apiRequest } from "../../actions/api.actions";
-import { setLoader } from '../../actions/ui.actions';
+import { API_SUCCESS, API_ERROR, apiRequest } from "../../actions/api.actions";
 import { API } from '../../constants/constants';
+import { setNotification } from '../../actions/notification.actions';
 
 export const usersMiddleware = ({dispatch}) => next => action => {
     next(action);
 
     switch (action.type) {
         case FETCH_USERS:
-            dispatch(apiRequest(null, 'GET', API.USERS, USERS));
-            dispatch(setLoader(true));
+            dispatch(apiRequest({body: null, method: 'GET', url: API.USERS, entity: USERS}));
             break;
         case `${USERS} ${API_SUCCESS}`:
-            dispatch(setUsers(action.payload));
-            dispatch(setLoader(false));
+            dispatch(setUsers({ users: action.payload }));
             break;
+        case `${USERS} ${API_ERROR}`:
+            dispatch(setNotification({
+                message: action.payload.message,
+                entity: USERS
+            }));
     }
 };
