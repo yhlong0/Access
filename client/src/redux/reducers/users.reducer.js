0c87cfb8502ad19a905cb6d705654842f9ea212b
yhlong0 <yhlong2@gmail.com>
2018-09-21 22:46:10 -0500

    add users reducer

--- client/src/redux/reducers/users.reducer.js
+++ client/src/redux/reducers/users.reducer.js
@@ -1,12 +1,13 @@
 import { SET_USERS } from '../actions/users.actions';
 
-export const userReducer = (users = [], action) => {
+const initState = [];
+
+export const usersReducer = (users = initState, action) => {
     const { payload } = action;
 
     switch (action.type) {
         case SET_USERS:
             return payload.data;
-    
         default:
             return users;
     }
