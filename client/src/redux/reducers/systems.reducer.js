f9b619949c08288d93ed6e14bd4ac832b2eb07db
Hailong Yang <yhlong2@gmail.com>
2018-09-22 11:24:24 -0500

    write reducer as function

--- client/src/redux/reducers/systems.reducer.js
+++ client/src/redux/reducers/systems.reducer.js
@@ -2,7 +2,16 @@ import { SET_SYSTEMS } from '../actions/systems.actions';
 
 const initState = [];
 
-export const systemsReducer = (systems = initState, action) => {
+// export const systemsReducer = (systems = initState, action) => {
+//     switch (action.type) {
+//         case SET_SYSTEMS:
+//             return action.payload;
+//         default:
+//             return systems;
+//     }
+// }
+
+export default function systemsReducer(systems = initState, action) {
     switch (action.type) {
         case SET_SYSTEMS:
             return action.payload;
