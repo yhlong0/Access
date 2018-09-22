b1e9e773f9eaeae13480c34a00e82e076e045643
Hailong Yang <yhlong2@gmail.com>
2018-09-22 11:23:59 -0500

    update typo

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
 
-import rolesReducer from './roles.reducer;
-import systemsReducer from './systems.reducer;
-import usersReducer from './users.reducer;
+import rolesReducer from './roles.reducer';
+import systemsReducer from './systems.reducer';
+import usersReducer from './users.reducer';
 
 
 export default combineReducers({
