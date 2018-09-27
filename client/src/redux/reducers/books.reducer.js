774ba8c06e2b0a260c926d6ec2d74e22501c20c6
yhlong0 <yhlong2@gmail.com>
2018-09-26 22:31:19 -0500

    modify book reducer.

--- client/src/redux/reducers/books.reducer.js
+++ client/src/redux/reducers/books.reducer.js
@@ -1,6 +1,6 @@
 import { SET_BOOKS } from '../actions/books.action';
 
-const initState = ['TextTrackCueList'];
+const initState = {};
 
 export default function booksReducer(books = initState, action) {
     switch (action.type) {
