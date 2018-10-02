c2b29a91f4ffc109ecd8c96336744b09125ca36b
yhlong0 <yhlong2@gmail.com>
2018-10-01 20:55:16 -0500

    modify dialog open

--- client/src/redux/middleware/app/dialog.js
+++ client/src/redux/middleware/app/dialog.js
@@ -1,15 +1,13 @@
 import { apiRequesst, API_SUCCESS, API_ERROR } from '../../actions/api.action';
 import { DIALOG, DIALOG_OPEN, closeDialog } from '../../actions/dialog.action';
+import { switchModalView } from '../../actions/ui.actions';
 
 import { API } from '../../constants/constants';
-import { switchModalView } from '../../actions/ui.actions';
 
 export const dialogMiddleware = () => next => action => {
     switch (action.type) {
         case DIALOG_OPEN:
-            next(switchModalView({
-
-            }));
+            next(switchModalView(DIALOG));
             next(apiRequesst({
                 body: null,
                 method: 'GET',
